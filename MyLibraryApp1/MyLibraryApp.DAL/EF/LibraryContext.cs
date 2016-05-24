b2cdc18bf007783b687026a4f96bcc9cb0f40e08
braginskyy <braginskyy_oleg@ukr.net>
2016-05-24 10:05:38 +0300

    Add new project
    MyLibraryApp

---
+++ MyLibraryApp1/MyLibraryApp.DAL/EF/LibraryContext.cs
@@ -0,0 +1,23 @@
+﻿using MyLibraryApp.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibraryApp.DAL.EF
+{
+    public class LibraryContext : DbContext
+    {        
+        public LibraryContext()
+            : base("MainConnectionStrind")
+        {
+            Database.SetInitializer<LibraryContext>(new MigrateDatabaseToLatestVersion<LibraryContext, Migrations.Configuration>());
+        }       
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<History> Historys { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
